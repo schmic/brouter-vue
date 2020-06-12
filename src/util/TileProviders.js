a572a97565c0af23369ee681517a7812591b8e80
Michael Scherer <mic@n49e08.de>
2020-06-13 00:16:29 +0200

    back to osm layer

--- src/util/TileProviders.js
+++ src/util/TileProviders.js
@@ -1,14 +1,14 @@
 const TileProviders = [
     {
         name: 'Cycloosm',
-        visible: true,
+        visible: false,
         url: 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png',
         attribution:
             'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     },
     {
         name: 'OpenStreetMap',
-        visible: false,
+        visible: true,
         attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
         url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     },
