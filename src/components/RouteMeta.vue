4cbb92cc2d10643af28450b6ab174c0cbff2f711
Michael Scherer <mic@n49e08.de>
2020-06-15 16:44:04 +0200

    route name input share thingy

--- src/components/RouteMeta.vue
+++ src/components/RouteMeta.vue
@@ -4,7 +4,21 @@
             Route
         </div>
         <div class="message-body">
-            <h2>Name: TODO</h2>
+            <div class="field has-addons">
+                <div class="control is-expanded">
+                    <input class="input" type="text" placeholder="Route Name" />
+                </div>
+                <div class="control">
+                    <button class="button is-primary">
+                        <span class="icon" title="Save"> <i class="fa fa-save"></i></span>
+                    </button>
+                </div>
+                <div class="control">
+                    <button class="button is-primary">
+                        <span class="icon" title="Share"> <i class="fa fa-share"></i></span>
+                    </button>
+                </div>
+            </div>
         </div>
     </article>
 </template>
