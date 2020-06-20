c615011af217d655acb9edaad1b33bd3d60a687d
Michael Scherer <mic@n49e08.de>
2020-06-20 11:12:18 +0200

    add Route view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Map from '../views/Map.vue';
+import Route from '../views/Route.vue';
 
 Vue.use(VueRouter);
 
@@ -16,6 +17,11 @@ const routes = [
         path: '/map',
         name: 'Map',
         component: Map
+    },
+    {
+        path: '/route/:name?',
+        name: 'Route',
+        component: Route
     }
     // {
     //     path: '/about',
---
+++ src/views/Route.vue
@@ -0,0 +1,128 @@
+<template>
+    <div class="container">
+        <section class="hero">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">
+                        Routes
+                    </h1>
+                    <h2 class="subtitle">
+                        ...
+                    </h2>
+                </div>
+            </div>
+        </section>
+        <article v-if="routes.length == 0" class="message is-info">
+            <div class="message-header">
+                <p>Hint</p>
+            </div>
+            <div class="message-body">
+                You did not save any route yet. <br />
+                Got to the <a href="#" class="href" @click="$router.push('map')"> Map view</a>, create one and then save
+                it.
+            </div>
+        </article>
+        <div v-for="(route, index) in routes.reverse()" :key="index" class="card">
+            <header class="card-header">
+                <p class="card-header-title">
+                    {{ route }}
+                </p>
+                <a href="#" class="card-header-icon" aria-label="more options">
+                    <span class="icon">
+                        <i class="fas fa-angle-down" aria-hidden="true"></i>
+                    </span>
+                </a>
+            </header>
+            <div class="card-content">
+                <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                    <br />
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item" @click="onRouteLoad(route)">Load</a>
+                <a href="#" class="card-footer-item" @click="onRouteDelete(route)">Delete</a>
+            </footer>
+        </div>
+        <div id="modal-import" class="modal" :class="{ 'is-active': modal.import.show }">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Import Route</p>
+                    <button class="delete" aria-label="close" @click="importModalHide"></button>
+                </header>
+                <section class="modal-card-body left-align">
+                    <div class="field">
+                        <h1><span class="strong"></span>Attention, this will replace your current route!</h1>
+                    </div>
+                    <div class="field">
+                        <label class="label">Name</label>
+                        <div class="control">
+                            <input
+                                v-model="modal.import.trackname"
+                                class="input"
+                                type="text"
+                                placeholder="Import route as ..."
+                            />
+                        </div>
+                    </div>
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button is-success" @click="importRouteTrigger">
+                        Import
+                    </button>
+                </footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            routes: [],
+            modal: {
+                import: {
+                    show: false,
+                    trackname: undefined
+                }
+            }
+        };
+    },
+    created() {
+        if (this.$route.query.share) {
+            this.importModalShow();
+        }
+        this.routes = JSON.parse(localStorage.getItem('routes') || '[]');
+    },
+    methods: {
+        onRouteLoad(route) {
+            console.log('route', route);
+            this.$store.dispatch('stateRestore', route);
+            this.$router.push('map');
+        },
+        onRouteDelete(route) {
+            // TODO: move logic to store
+            this.routes = JSON.parse(localStorage.getItem('routes') || '[]').filter(_route => route != _route);
+            localStorage.setItem('routes', JSON.stringify(this.routes));
+            ['waypoints', 'nogos', 'pois', 'trackname'].forEach(it => localStorage.removeItem(`${route}/${it}`));
+        },
+        importModalShow() {
+            this.modal.import.trackname = this.$route.query.trackname
+                ? this.$route.query.trackname
+                : `Import - ` + new Date().toISOString();
+            this.modal.import.show = true;
+        },
+        importModalHide() {
+            this.modal.import.show = false;
+        },
+        importRouteTrigger() {
+            console.log('IMPORT', this.$route.query);
+        }
+    }
+};
+</script>
+
+<style></style>
