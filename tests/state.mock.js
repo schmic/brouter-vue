932ade2ff2b8e1d1d9570b29a771e8e2330a654f
Michael Scherer <mic@n49e08.de>
2020-06-25 22:39:46 +0200

    bigger state mock for tests

--- tests/state.mock.js
+++ tests/state.mock.js
@@ -1,51 +1,73 @@
-export default {
+const state = {
+    trackname: 'West East',
+    alternativeIdx: 0,
+    profile: 'fastbike',
     waypoints: [
         {
-            id: 126,
-            latlng: { lat: 49.678107, lng: 8.617215, alt: 103 },
+            id: 419,
+            latlng: { lat: 49.638158, lng: 8.418889, alt: 90 },
             options: {
                 type: 'waypoint',
-                icon: {
-                    options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }
-                },
+                icon: { options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }, _initHooksCalled: true },
                 colorName: 'blue'
             }
         },
         {
-            id: 132,
-            latlng: { lat: 49.768117, lng: 8.648836, alt: 138 },
+            id: 422,
+            latlng: { lat: 49.650839, lng: 8.530293, alt: 95.75 },
             options: {
                 type: 'waypoint',
-                icon: {
-                    options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }
-                },
+                icon: { options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }, _initHooksCalled: true },
                 colorName: 'blue'
             }
         },
         {
-            id: 140,
-            latlng: { lat: 49.791388, lng: 8.792485, alt: 240.5 },
+            id: 428,
+            latlng: { lat: 49.67951, lng: 8.619322, alt: 104.75 },
             options: {
                 type: 'waypoint',
-                icon: {
-                    options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }
-                },
+                icon: { options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }, _initHooksCalled: true },
+                colorName: 'blue'
+            }
+        },
+        {
+            id: 433,
+            latlng: { lat: 49.718229, lng: 8.730714, alt: 357 },
+            options: {
+                type: 'waypoint',
+                icon: { options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }, _initHooksCalled: true },
+                colorName: 'blue'
+            }
+        },
+        {
+            id: 438,
+            latlng: { lat: 49.695033, lng: 8.771771, alt: 449.5 },
+            options: {
+                type: 'waypoint',
+                icon: { options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }, _initHooksCalled: true },
+                colorName: 'blue'
+            }
+        },
+        {
+            id: 443,
+            latlng: { lat: 49.702981, lng: 8.839804, alt: 314 },
+            options: {
+                type: 'waypoint',
+                icon: { options: { icon: 'map-signs', markerColor: 'blue', prefix: 'fa' }, _initHooksCalled: true },
                 colorName: 'blue'
             }
         }
     ],
     nogos: [
-        {
-            id: 152,
-            name: 'TODO: NoGo name',
-            latlng: { lat: 49.641356, lng: 8.689295 },
-            radius: 2500
-        },
-        {
-            id: 171,
-            name: 'TODO: NoGo name',
-            latlng: { lat: 49.709378, lng: 8.724315 },
-            radius: 2500
-        }
-    ]
+        { id: 448, name: 'TODO: NoGo name', latlng: { lat: 49.654321, lng: 8.654321 }, radius: 2500 },
+        { id: 449, name: 'TODO: NoGo name', latlng: { lat: 49.123456, lng: 8.123456 }, radius: 1200 }
+    ],
+    pois: [
+        { id: 450, name: 'TODO:  Name', latlng: { lat: 49.67243245469188, lng: 8.698514726319036 } },
+        { id: 453, name: 'TODO:  Name', latlng: { lat: 49.696871366603204, lng: 8.522688759643914 } },
+        { id: 456, name: 'TODO:  Name', latlng: { lat: 49.61951348553559, lng: 8.48491364961603 } }
+    ],
+    stats: { distance: 44446, totaltime: 11060, formattedtime: '3:04', ascend: 782, descend: 556 }
 };
+
+export default state;
--- tests/unit/BRouter.spec.js
+++ tests/unit/BRouter.spec.js
@@ -4,8 +4,8 @@ import state from '../state.mock';
 
 const url = {
     parts: {
-        lonlats: '&lonlats=8.617215,49.678107|8.648836,49.768117',
-        nogos: '&nogos=8.689295,49.641356,2500|8.724315,49.709378,2500',
+        lonlats: '&lonlats=8.418889,49.638158|8.530293,49.650839',
+        nogos: '&nogos=8.654321,49.654321,2500|8.123456,49.123456,1200',
         shared:
             'cHJvZmlsZT1mYXN0YmlrZSZhbHRlcm5hdGl2ZWlkeD0wJmZvcm1hdD1nZW9qc29uJm5vZ29zPTguNjg5Mjk1LDQ5LjY0MTM1NiwyNTAwfDguNzI0MzE1LDQ5LjcwOTM3OCwyNTAwJmxvbmxhdHM9OC42MTcyMTUsNDkuNjc4MTA3fDguNjQ4ODM2LDQ5Ljc2ODExNw'
     }
@@ -19,7 +19,7 @@ describe('BRouter', () => {
     beforeEach(() => {});
 
     it('state is correct', () => {
-        expect(state.waypoints.length).toBe(3);
+        expect(state.waypoints.length).toBe(6);
         expect(state.nogos.length).toBe(2);
     });
 
