f9fe63e4da3632db2a1c0d12041de4a36609defc
Michael Scherer <mic@n49e08.de>
2020-07-02 19:18:48 +0200

    segment tooltip

---
+++ src/components/tooltip/Segment.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="level">
+        <div class="level-item" style="margin: 0.5em"><i class="fa fa-route"> </i>${segment.distance}km</div>
+        <div class="level-item" style="margin: 0.5em"><i class="fa fa-angle-double-up"> </i>${segment.ascend}m</div>
+        <div class="level-item" style="margin: 0.5em"><i class="fa fa-angle-double-down"> </i>${segment.descend}m</div>
+    </nav>
+</template>
+
+<script>
+export default {
+    props: {
+        segment: {
+            type: Object,
+            required: true
+        }
+    }
+};
+</script>
